981376966d759eef1a822e92b8a870a3b400432d
kaio keinner <keinnerkaio@gmail.com>
2023-08-23 19:21:27 -0400

    criando entidades

---
+++ ecmmerce/ecmmerce/src/main/java/com/tads4/ecmmerce/EcmmerceApplication.java
@@ -0,0 +1,13 @@
+package com.tads4.ecmmerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcmmerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcmmerceApplication.class, args);
+	}
+
+}
---
+++ ecmmerce/ecmmerce/src/test/java/com/tads4/ecmmerce/EcmmerceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tads4.ecmmerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcmmerceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
